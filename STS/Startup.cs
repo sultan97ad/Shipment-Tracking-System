74a3ebd9f6ca6752b377456c232453aaa57d09dd
sultan97ad <44210447+sultan97ad@users.noreply.github.com>
2020-05-22 22:21:01 +0300

    Add project files.

---
+++ STS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(STS.Startup))]
+namespace STS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
