b619f0436a03de5ea5c812e2e95121246c11578a
sultan97ad <44210447+sultan97ad@users.noreply.github.com>
2020-08-17 19:22:16 +0300

    Adding Basic Shipments statistics

---
+++ STS/Controllers/StatisticsController.cs
@@ -0,0 +1,70 @@
+﻿using STS.Models;
+using STS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STS.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private ApplicationDbContext DbContext;
+
+        public StatisticsController()
+        {
+            DbContext = ApplicationDbContext.Create();
+        }
+
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View(GenerateStatisticsIndexViewModel());
+        }
+
+        #region Helpers
+
+        private StatisticsIndexViewModel GenerateStatisticsIndexViewModel()
+        {
+            var ViewModel = new StatisticsIndexViewModel
+            {
+                RegisteredShipments = GetRegisteredShipments(),
+                ShipmentsWaitingShipping = GetShipmentsWaitingShipping(),
+                ShipmentsWaitingCollection = GetShipmentsWaitingCollection(),
+                CollectedShipments = GetCollectedShipments()
+            };
+            return ViewModel;
+        }
+
+        private string GetCollectedShipments()
+        {
+            return DbContext.Shipments.Where(Shipment => Shipment.Status == (byte)Status.Collected).Count().ToString();
+        }
+
+        private string GetShipmentsWaitingCollection()
+        {
+            return DbContext.Shipments.Where(Shipment => Shipment.Status == (byte)Status.WaitingCollection).Count().ToString();
+        }
+
+        private string GetShipmentsWaitingShipping()
+        {
+            return DbContext.Shipments.Where(Shipment => Shipment.Status == (byte)Status.WaitingShipping ).Count().ToString();
+        }
+
+        private string GetRegisteredShipments()
+        {
+            return DbContext.Shipments.Count().ToString();
+        }
+
+        enum Status
+        {
+            WaitingShipping,
+            Shipping,
+            WaitingCollection,
+            Collected
+        }
+
+        #endregion
+    }
+}
---
+++ STS/ViewModels/StatisticsViewModels.cs
@@ -0,0 +1,18 @@
+﻿using STS.Models;
+using STS.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace STS.ViewModels
+{
+    public class StatisticsIndexViewModel
+    {
+        [Display(Name = "RegisteredShipments", ResourceType = typeof(Resources.Views.Statistics))]
+        public string RegisteredShipments { set; get; }
+        [Display(Name = "ShipmentsWaitingShipping", ResourceType = typeof(Resources.Views.Statistics))]
+        public string ShipmentsWaitingShipping { set; get; }
+        [Display(Name = "ShipmentsWaitingCollection", ResourceType = typeof(Resources.Views.Statistics))]
+        public string ShipmentsWaitingCollection { set; get; }
+        [Display(Name = "CollectedShipments", ResourceType = typeof(Resources.Views.Statistics))]
+        public string CollectedShipments { set; get; }
+    }
+}
